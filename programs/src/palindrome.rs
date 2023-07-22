37b513a57e39bfe25d9f9fc79f3eb73f3ab5fbb3
Fenn Saji <fenn@swnglobal.com>
2023-07-22 16:55:59 +0530

    Palindrome another version added

--- programs/src/palindrome.rs
+++ programs/src/palindrome.rs
@@ -2,3 +2,18 @@ pub fn palindrome(text: &str) -> bool {
     let rev_text = text.chars().rev().collect::<String>();
     text == rev_text
 }
+
+pub fn is_palindrome(x: i32) -> bool {
+    if x < 0 {
+        return false; // Negative numbers are not palindromes
+    }
+
+    let mut d = x;
+    let mut r = 0;
+    while d != 0 {
+        let m = d % 10;
+        r = r * 10 + m;
+        d = d / 10;
+    }
+    x == r
+}
