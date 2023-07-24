a426496f19518b14eb2bdaa8e30dc90a126fb25e
Fenn Saji <fenn@swnglobal.com>
2023-07-24 21:19:31 +0530

    Improved valid paran a bit

--- programs/src/valid_paranthesis.rs
+++ programs/src/valid_paranthesis.rs
@@ -1,29 +1,28 @@
 use std::collections::HashMap;
 
 pub fn is_valid(s: String) -> bool {
-    let possible_brackets: HashMap<char, char> = HashMap::from([
-        ('(', ')'),
-        ('{', '}'),
-        ('[', ']')
-    ]);
-    let mut stack: Vec<char> = vec![];
-    let s_chars = s.chars();
-    if s.len() % 2 != 0 {
+  let possible_brackets: HashMap<char, char> = HashMap::from([
+    ('(', ')'),
+    ('{', '}'),
+    ('[', ']')
+  ]);
+  let mut stack: Vec<char> = vec![];
+  let s_chars = s.chars();
+  if s.len() % 2 != 0 {
+    return false;
+  }
+  for c in s_chars {
+    if let Some(closing_bracket) = possible_brackets.get(&c) {
+      stack.push(*closing_bracket);
+    } else {
+      let popped_item = stack.pop();
+      if popped_item.is_none() {
         return false;
+      }
+      if popped_item.unwrap() != c {
+        return false;
+      }
     }
-    for c in s_chars {
-        if possible_brackets.contains_key(&c) {
-            stack.push(c);
-        } else {
-            let popped_item = stack.pop();
-            if popped_item.is_none() {
-                return false;
-            }
-            let opposite_bracket = possible_brackets.get(&popped_item.unwrap());
-            if *opposite_bracket.unwrap() != c {
-                return false;
-            }
-        }
-    }
-    stack.len() == 0
+  }
+  stack.len() == 0
 }
