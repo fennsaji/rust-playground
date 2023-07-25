9e8463ac5e96119e5b2e976e4bf0e0c449c20423
Fenn Saji <fenn@swnglobal.com>
2023-07-25 19:28:25 +0530

    Updated check duplicates

--- programs/src/check_duplicates.rs
+++ programs/src/check_duplicates.rs
@@ -1,8 +1,22 @@
+// Memory optimised
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
   for (i, n) in nums.iter().enumerate() {
       if let Some(_) = nums.iter().skip(i+1).find(|&x| x==n) {
           return true;
       }
   }
+  false
+}
+
+// Time Optimised
+pub fn contains_duplicate_to(nums: Vec<i32>) -> bool {
+  let mut seen_numbers = HashSet::new();
+
+  for num in nums {
+      if !seen_numbers.insert(num) {
+          return true;
+      }
+  }
+
   false
 }
