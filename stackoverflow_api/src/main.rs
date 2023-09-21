348f1081d9d6756470b09c5bd728a7863e414ad3
Fenn Saji <fenn@swnglobal.com>
2023-09-21 18:48:11 +0530

    Updated to stage 2

--- stackoverflow_api/src/main.rs
+++ stackoverflow_api/src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
+// TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+
 mod cors;
 mod handlers;
 mod models;
@@ -10,6 +12,24 @@ use handlers::*;
 
 #[launch]
 async fn rocket() -> _ {
+    // TODO: Initialize pretty_env_logger
+    // TODO: Initialize dotenv
+
+    // Create a new PgPoolOptions instance with a maximum of 5 connections.
+    // Use dotenv to get the database url. 
+    // Use the `unwrap` or `expect` method instead of handling errors. If an
+    // error occurs at this stage the server should be terminated. 
+    // See examples on GitHub page: https://github.com/launchbadge/sqlx
+    // let pool = todo!();
+
+    // Using slqx, execute a SQL query that selects all questions from the questions table.
+    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
+    // make sure we can connect to the database.  
+    // let recs = todo!();
+
+    info!("********* Question Records *********");
+    // TODO: Log recs with debug formatting using the info! macro
+
     rocket::build()
         .mount(
             "/",
