b3c43a318360fd1a19a7a064fbea54f589975752
Fenn Saji <fenn@swnglobal.com>
2023-09-21 17:19:00 +0530

    Updated models and route for answers

--- stackoverflow_api/src/handlers/mod.rs
+++ stackoverflow_api/src/handlers/mod.rs
@@ -29,13 +29,31 @@ pub async fn delete_question(
 //       The handler function should be called `create_answer`.
 //       
 //       hint: this function should look very similar to the create_question function above
+#[post("/answer", data = "<answer>")]
+pub async fn create_answer(
+    answer: Json<Answer>,
+) -> Json<AnswerDetail> {
+    todo!()
+}
 
 // TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
 //       The handler function should be called `read_answers`.
 //       
 //       hint: this function should look very similar to the read_questions function above
+#[get("/answers", data = "<question_id>")]
+pub async fn read_answers(
+    question_id: Json<QuestionId>,
+) -> Json<AnswerDetail> {
+    todo!()
+}
 
 // TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
 //       The handler function should be called `delete_answer`.
 //       
 //       hint: this function should look very similar to the delete_question function above
+#[delete("/answer", data = "<answer_id>")]
+pub async fn delete_answer(
+    answer_id: Json<AnswerId>,
+) -> Json<AnswerDetail> {
+    todo!()
+}
