6e1175d732d5e2affe6eda3bb38e8a5ac5db7cc6
Fenn Saji <fenn@swnglobal.com>
2023-09-22 18:52:31 +0530

    Added Stage 2 step 2 template

--- stackoverflow_api/src/main.rs
+++ stackoverflow_api/src/main.rs
@@ -1,34 +1,43 @@
 #[macro_use]
 extern crate rocket;
 
-// TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
+
+use dotenvy::dotenv;
+
+use sqlx::postgres::PgPoolOptions;
 
 mod cors;
 mod handlers;
 mod models;
+mod persistance;
 
 use cors::*;
 use handlers::*;
 
 #[launch]
 async fn rocket() -> _ {
-    // TODO: Initialize pretty_env_logger
-    // TODO: Initialize dotenv
+    pretty_env_logger::init();
+    dotenv().ok();
 
-    // Create a new PgPoolOptions instance with a maximum of 5 connections.
-    // Use dotenv to get the database url. 
-    // Use the `unwrap` or `expect` method instead of handling errors. If an
-    // error occurs at this stage the server should be terminated. 
-    // See examples on GitHub page: https://github.com/launchbadge/sqlx
-    // let pool = todo!();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+    
+    // TODO: Delete this query
+    let recs = sqlx::query!("SELECT * FROM questions")
+            .fetch_all(&pool)
+            .await
+            .unwrap();
 
-    // Using slqx, execute a SQL query that selects all questions from the questions table.
-    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
-    // make sure we can connect to the database.  
-    // let recs = todo!();
-
+    // TODO: Delete these log statements
     info!("********* Question Records *********");
-    // TODO: Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
 
     rocket::build()
         .mount(
